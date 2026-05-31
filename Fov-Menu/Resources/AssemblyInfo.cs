b3e9197ae85a6dee1e76f5c0aec7624d3427650e
Mandsdel <morten.mansdal@gmail.com>
2026-05-31 17:08:36 +0200

    Fix target framework and Windows platform support

--- Fov-Menu/Resources/AssemblyInfo.cs
+++ Fov-Menu/Resources/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Versioning;
 using System.Windows;
 
+[assembly: SupportedOSPlatform("windows")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
